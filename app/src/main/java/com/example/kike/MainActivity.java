dfc0ccc454d0b9b32b63c428f75f8e307936a027
kekikike <enriquequeq6@gmail.com>
2026-09-19 17:40:05 -0400

    Primer commit

---
+++ app/src/main/java/com/example/kike/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.kike;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText CAJA;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        CAJA=this.findViewById(R.id.CAJA);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    //Login
+    public void Login (View view){
+        Intent obj=new Intent(this,PantallaDos.class);
+        String usuario = CAJA.getText().toString();
+        obj.putExtra("USUARIO", usuario);
+        this.startActivity(obj);
+        this.finish();
+    }
+
+}
---
+++ app/src/main/java/com/example/kike/PantallaDos.java
@@ -0,0 +1,32 @@
+package com.example.kike;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PantallaDos extends AppCompatActivity {
+
+    TextView Etiqueta;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_pantalla_dos);
+
+        Etiqueta = this.findViewById(R.id.Etiqueta);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        String usuario = this.getIntent().getStringExtra("USUARIO");
+        Etiqueta.setText("Bievenido!!!" + usuario);
+    }
+
+}
